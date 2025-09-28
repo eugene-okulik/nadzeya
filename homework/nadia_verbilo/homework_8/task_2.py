668b3fe9dff5fd4b7329c519e8c0f60b3228c933
Nadia Verbilo <test.email@gmail.com>
2025-09-28 13:01:05 +0300

    Homework_8 done

---
+++ homework/nadia_verbilo/homework_8/task_2.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    fib1, fib2 = 0, 1
+    for i in range(n):
+        fib1, fib2 = fib2, fib1 + fib2
+        yield fib1
+
+def print_fibonacci(n):
+    my_list = list(fibonacci(n))
+    print(my_list[-1])
+
+print_fibonacci(5)
+print_fibonacci(200)
+print_fibonacci(1000)
+# print_fibonacci(100000)
