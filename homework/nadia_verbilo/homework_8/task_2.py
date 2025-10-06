569445c22d3c0e920abf46c8904a9beda7b55419
Nadia Verbilo <test.email@gmail.com>
2025-10-06 17:44:03 +0300

    Homework_8 task_2 redone

--- homework/nadia_verbilo/homework_8/task_2.py
+++ homework/nadia_verbilo/homework_8/task_2.py
@@ -1,3 +1,6 @@
+import random
+
+
 def fibonacci(n):
     fib1, fib2 = 0, 1
     for i in range(n):
@@ -5,12 +8,8 @@ def fibonacci(n):
         yield fib1
 
 
-def print_fibonacci(n):
-    my_list = list(fibonacci(n))
-    print(my_list[-1])
+def my_gen():
+    yield random.randrange(1, 5)
+    for num in my_gen():
+        print(num)
 
-
-print_fibonacci(5)
-print_fibonacci(200)
-print_fibonacci(1000)
-# print_fibonacci(100000)
